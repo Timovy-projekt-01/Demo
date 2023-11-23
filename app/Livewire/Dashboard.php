b24630e2337d60ebdc468257ed38cc7b0f9eafe0
Michal Vrbovsky <100531156+VrbovskyM@users.noreply.github.com>
2023-11-23 14:46:09 +0100

    fixed showing entity after clicking on search

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -8,7 +8,7 @@ use App\Services\Malware\Sparql;
 class Dashboard extends Component
 {
     public $filter;
-    public $filterItems;
+    public $filterItems = '';
     public $items;
     public $counter = 0;
     public $entitiesFromSearch = '';
@@ -21,35 +21,15 @@ class Dashboard extends Component
     }
     public function render()
     {
-        //filterItems is the value of the input field
-        $this->filter = '%' . $this->filterItems . '%';
-        //$this->items = Item::where('name', 'like', $this->filter)->get();
-        $this->counter = $this->counter + 1;
         if($this->filterItems != ''){
             $this->entitiesFromSearch = $this->sparql->searchEntities($this->filterItems);
-
         }
         return view('livewire.dashboard');
     }
 
-    public function show(){
-        dd("couter = " . $this->counter, "filterItems= " . $this->filterItems, "filter= " . $this->filter, "items=",$this->items);
-    }
-
-    private function extractId($uri, $charToFind = '#')
-    {
-        if ($uri) {
-            $hashtagIndex = strrpos($uri, $charToFind);
-            return substr($uri, $hashtagIndex + 1);
-        }
-        return '';
-    }
     public function showEntireEntity($malwareId) {
-        dd("IDK");
-        dump($malwareId);
 
         $this->properties = $this->sparql->getMalwareProperties($malwareId);
-        dump($this->properties, "idk");
-        //return view('livewire.dashboard');
+        $this->filterItems = '';
     }
 }
