e640cdf677f98b2c93f331fa1455a3353c4d495b
Michal Vrbovsky <100531156+VrbovskyM@users.noreply.github.com>
2023-12-06 21:04:40 +0100

    search bar designed

--- resources/views/livewire/search-history.blade.php
+++ resources/views/livewire/search-history.blade.php
@@ -1,28 +1,37 @@
 <div>
-    <div x-data="{ delHistory: function() {
-        console.log('clearHistory');
-        localStorage.removeItem('searchHistory');
-        $wire.history = [];
-        console.log($wire.history);
-        $wire.dispatch('re-render');
-        }}"
-        x-init="
-            let history = JSON.parse(localStorage.getItem('searchHistory')) || [];
-            console.log(history);
-            $wire.history = history;">
-        <button @click="delHistory">Clear History</button>
-        @forelse ($history as $item)
-            <p>
-                {{ $item['hasName'] }}
-            </p>
-            <p>
-                {{ $item['hasId'] }}
-            </p>
-        @empty
-            <p>
-                No history
-            </p>
-        @endforelse
+    <div x-data="{
+        delHistory: function() {
+            console.log('clearHistory');
+            localStorage.removeItem('searchHistory');
+            $wire.history = [];
+            console.log($wire.history);
+            $wire.dispatch('re-render');
+        }
+    }" x-init="let history = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    console.log(history);
+    $wire.history = history;">
+        <div class="flex flex-col ">
+            <h3 class="text-xl font-bold text-center border-b-2 border-slate-600 pb-3">Search History</h3>
+            <div class="py-3 divide-y">
+                @forelse ($history as $item)
+                <div class="transition-transform duration-500 ease-in-out hover:underline border-b py-2 cursor-pointer transform hover:translate-x-1">
+                    <p class="font-mono  ">
+                        {{ $item['hasName'] }}
+                    </p>
+                    <p class="font-mono text-sm text-gray-600 break-all">
+                        {{ $item['hasId'] }}
+                    </p>
+                </div>
+                @empty
+                <p>
+                    No history
+                </p>
+                @endforelse
+            </div>
+            <div class="text-sm text-center bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded">
+                <button @click="delHistory">Clear History</button>
+            </div>
+        </div>
     </div>
     @script
         <script>
--- resources/views/main-page.blade.php
+++ resources/views/main-page.blade.php
@@ -1,13 +1,17 @@
 @extends('components.layouts.app')
 
 @section('content')
-    <div class="w-4/5 mx-auto">
-        <div class="container flex flex-col items-center  justify-center my-5">
-            <livewire-search-bar />
+    <div class="flex my-5 mx-auto">
+        <div class="xl:container mx-auto flex w-full px-5 gap-5">
 
-            <livewire-render-entity />
+            <div class="basis-2/12 py-5 px-3 shadow-lg h-fit">
+                <livewire-search-history />
+            </div>
+            <div class="basis-4/5 w-full flex flex-col ">
+                <livewire-search-bar />
+                <livewire-render-entity />
+            </div>
 
-            <livewire-search-history />
         </div>
     </div>
     @livewireScripts
