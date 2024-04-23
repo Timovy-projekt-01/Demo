01ff4578b064294ccccf009457a78c683326ef69
Miroslav Harmady 111273 <MirkoGimi@gmail.com>
2024-04-23 21:42:54 +0200

    feat: create middleware for registration

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,5 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'lang' => \App\Http\Middleware\LanguageManager::class,
+        'superAdmin' => \App\Http\Middleware\CheckSuperAdmin::class,
     ];
 }
--- routes/auth.php
+++ routes/auth.php
@@ -5,8 +5,10 @@ use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
 Route::middleware('guest')->group(function () {
+
     Volt::route('register', 'pages.auth.register')
-        ->name('register');
+        ->name('register')
+        ->middleware('superAdmin');
 
     Volt::route('login', 'pages.auth.login')
         ->name('login');
