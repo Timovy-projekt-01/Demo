8d237da2c74912fb849d6aed0afd897f0b31264c
Richard Körösi <korosi.richard@gmail.com>
2024-05-14 10:50:42 +0200

    refactor: make code more readable

--- tests/Unit/BasicTest.php
+++ tests/Unit/BasicTest.php
@@ -33,68 +33,32 @@ class BasicTest extends TestCase
 
     public function test_language_correspondence_on_home_page()
     {
-        // Set the language to English
-        $this->followingRedirects()->get(route('lang.switch', 'en'));
-
-        // Get the about page
-        $response = $this->get('/');
-
-        // Assert that the about page contains English text
-        $response->assertSee('Search History');
-
-        // Set the language to Slovak
-        $this->followingRedirects()->get(route('lang.switch', 'sk'));
-
-        // Get the about page
-        $response = $this->get('/');
-
-        // Assert that the about page contains Slovak text
-        $response->assertSee('História vyhľadávania');
+        $this->assertLanguageCorrespondence('Search History', 'História vyhľadávania', '/');
+    }
+    
+    public function test_language_correspondence_on_about_page()
+    {
+        $this->assertLanguageCorrespondence('About project', 'O projekte', '/about');
     }
 
-    public function test_language_correspondence_about_pages()
+    public function test_language_correspondence_on_update_page()
     {
-        // Set the language to English
-        $this->followingRedirects()->get(route('lang.switch', 'en'));
-
-        // Get the about page
-        $response = $this->get('/about');
-
-        // Assert that the about page contains English text
-        $response->assertSee('About project');
-
-        // Set the language to Slovak
-        $this->followingRedirects()->get(route('lang.switch', 'sk'));
-
-        // Get the about page
-        $response = $this->get('/about');
-
-        // Assert that the about page contains Slovak text
-        $response->assertSee('O projekte');
+        $this->assertLanguageCorrespondence('Update', 'Aktualizovať', '/update');
     }
-
-    public function test_api_endpoint_response()
+    
+    private function assertLanguageCorrespondence($englishText, $slovakText, $page)
     {
-        // Create a Guzzle HTTP client
-        $client = new \GuzzleHttp\Client();
-
-        // Send a GET request to the API endpoint
-        $response = $client->get('http://localhost:9999/bigdata/sparql', [
-            'query' => [
-                'query' => 'SELECT * WHERE {?s ?p ?o} LIMIT 1',
-            ],
-        ]);
-
-        // Assert the response status is 200 (OK)
-        $this->assertEquals(200, $response->getStatusCode());
+        $this->followingRedirects()->get(route('lang.switch', 'en'));
+        $response = $this->get($page);
+        $response->assertSee($englishText);
+        $this->followingRedirects()->get(route('lang.switch', 'sk'));
+        $response = $this->get($page);
+        $response->assertSee($slovakText);
     }
 
     public function test_api_response_with_existing_searched_term()
     {
-        // Create a Guzzle HTTP client
         $client = new \GuzzleHttp\Client();
-
-        // Send a GET request to the API endpoint
         $response = $client->get('http://localhost:9999/bigdata/sparql', [
             'query' => [
                 'query' => 'SELECT ?entity ?property ?value
@@ -147,10 +111,7 @@ class BasicTest extends TestCase
 
     public function test_api_response_with_non_existing_searched_term()
     {
-        // Create a Guzzle HTTP client
         $client = new \GuzzleHttp\Client();
-
-        // Send a GET request to the API endpoint
         $response = $client->get('http://localhost:9999/bigdata/sparql', [
             'query' => [
                 'query' => 'SELECT ?entity ?property ?value
